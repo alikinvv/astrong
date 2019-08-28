6a165e2ef3026645b4e520204b7e65ae353d8a41
Valery Alikin <weblcw@gmail.com>
2019-08-28 14:55:25 +0500

    update

--- build/js/main.js
+++ build/js/main.js
@@ -5,4 +5,107 @@ $('body').on('click' , '.navigation a[data-tab]', (e) => {
     $(`.main__item[data-tab="${$(e.currentTarget).attr('data-tab')}"]`).addClass('active');
 });
 
-$('.main').css('min-height', $('.main').height() + 66);
+$('body').on('click' , '.form__nav a[data-form]', (e) => {
+    $('.form__nav a').removeClass('active');
+    $(e.currentTarget).addClass('active');
+    $(`.form__body`).removeClass('active');
+    $(`.form__body[data-form="${$(e.currentTarget).attr('data-form')}"]`).addClass('active');
+});
+
+if ($(window).width() >= 768) {
+    $('.main').css('min-height', $('.main').height() + 66);
+}
+
+$('body').on('click', '.navigation__btn', () => {
+    $('.form__body.active input:first').focus();
+});
+
+$('body').on('submit', 'form', (e) => {
+    e.preventDefault();
+
+    if ($(e.currentTarget).hasClass('uri')) {
+        if ($('.uri input[name="organization"]').val() !== '' && $('.uri input[name="address"]').val() !== '' && $('.uri input[name="inn"]').val() !== '' && $('.uri input[name="phone"]').val() !== '' && $('.uri input[name="email"]').val() !== '') {
+            $.ajax({
+                type: "POST",
+                url: "../form.php",
+                data: $(e.currentTarget).serialize(),
+                success: () => {
+                    $('.modal').removeClass('active');
+                    $('.modals').addClass('active');
+                    $('.modal.success').addClass('active');
+                }					
+            });
+        } else {
+            if ($('.uri input[name="organization"]').val() === '') {
+                $('.uri input[name="organization"]').addClass('error');
+            }
+
+            if ($('.uri input[name="address"]').val() === '') {
+                $('.uri input[name="address"]').addClass('error');
+            }
+
+            if ($('.uri input[name="inn"]').val() === '') {
+                $('.uri input[name="inn"]').addClass('error');
+            }
+
+            if ($('.uri input[name="phone"]').val() === '') {
+                $('.uri input[name="phone"]').addClass('error');
+            }
+
+            if ($('.uri input[name="email"]').val() === '') {
+                $('.uri input[name="email"]').addClass('error');
+            }
+        }
+    } else if ($(e.currentTarget).hasClass('phy')) {
+        if ($('.phy input[name="fio"]').val() !== '' && $('.phy input[name="address"]').val() !== '' && $('.phy input[name="phone"]').val() !== '' && $('.phy input[name="email"]').val() !== '') {
+            $.ajax({
+                type: "POST",
+                url: "../form.php",
+                data: $(e.currentTarget).serialize(),
+                success: () => {
+                    $('.modal').removeClass('active');
+                    $('.modals').addClass('active');
+                    $('.modal.success').addClass('active');
+                }					
+            });
+        } else {
+            if ($('.phy input[name="fio"]').val() === '') {
+                $('.phy input[name="fio"]').addClass('error');
+            }
+
+            if ($('.phy input[name="address"]').val() === '') {
+                $('.phy input[name="address"]').addClass('error');
+            }
+
+            if ($('.phy input[name="phone"]').val() === '') {
+                $('.phy input[name="phone"]').addClass('error');
+            }
+
+            if ($('.phy input[name="email"]').val() === '') {
+                $('.phy input[name="email"]').addClass('error');
+            }
+        }
+    } else {
+        $.ajax({
+            type: "POST",
+            url: "../form.php",
+            data: $(e.currentTarget).serialize(),
+            success: () => {
+                $('.modal').removeClass('active');
+                $('.modals').addClass('active');
+                $('.modal.success').addClass('active');
+            }					
+        });
+    }
+});
+
+$('body').on('focus', 'input', (e) => {
+    $(e.currentTarget).removeClass('error');
+});
+
+$('input[name="phone"]').mask('+0 (000) 000-00-00', {placeholder: 'Телефон'});
+
+$('body').on('click', '.modal .close', (e) => {
+    $('.modals').removeClass('active');
+    $('.modal').removeClass('active');
+});
---
+++ src/form.php
@@ -0,0 +1,57 @@
+ 
+<?php 
+
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.yandex.ru';  																							// Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'phpmails@yandex.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = '$dk820&123'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
+
+$mail->setFrom('phpmails@yandex.ru'); // от кого будет уходить письмо?
+$mail->addAddress('lecaw@yandex.ru');     // Кому будет уходить письмо 
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+if ($_POST['hide'] === 'uri') {
+    $mail->Subject = 'Заявка юр. лица на вывоз ТКО с сайта Астронг';
+    $mail->Body    = 'Организация: ' . $_POST['organization'] . ', Адрес: ' . $_POST['address'] . ', ИНН: ' . $_POST['inn'] . ', Телефон: ' . $_POST['phone'] . ', Email: ' . $_POST['email']  . ', Комментарий: ' . $_POST['comment'];
+    
+}
+
+if ($_POST['hide'] === 'phy') {
+    $mail->Subject = 'Заявка физ. лица на вывоз ТКО с сайта Астронг';
+    $mail->Body    = 'ФИО: ' . $_POST['fio'] . ', Адрес: ' . $_POST['address'] . ', Телефон: ' . $_POST['phone'] . ', Email: ' . $_POST['email']  . ', Комментарий: ' . $_POST['comment'];
+}
+
+if ($_POST['hide'] === 'questions') {
+    $mail->Subject = 'Заявка с сайта Астронг';
+    $mail->Body    = 'Имя: ' . $_POST['name'] . ', Телефон: ' . $_POST['phone'] . ', Телефон: ';
+}
+
+if(isset($_FILES['attachfile'])) { 
+    if($_FILES['attachfile']['error'] == 0){ 
+        $mail->AddAttachment($_FILES['attachfile']['tmp_name'], $_FILES['attachfile']['name']); 
+    } 
+}
+
+$mail->AltBody = '';
+
+if(!$mail->send()) {
+    echo 'Error';
+} else {
+    echo 'success';
+}
+?>
